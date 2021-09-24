1ff5e800bda30161239b3454105d6de295ab6573
Björn Scheppler <scep@zhaw.ch>
2021-09-24 15:06:33 +0200

    Semesterwoche 3 abgeschlossen

---
+++ processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/ProcessApplicationRestController.java
@@ -0,0 +1,39 @@
+package ch.zhaw.gpi.twitterreviewprocessapplication;
+
+import java.util.HashMap;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ProcessApplicationRestController {
+    
+    @Autowired
+    private ProcessEngine processEngine;
+
+    @PostMapping(value="/handle-tweet-proposal")
+    public ResponseEntity<String> handleTweetProposal(TweetProposal tweetProposal) {
+        HashMap<String, Object> variables = new HashMap<>();
+        variables.put("anfrageStellenderBenutzer", tweetProposal.getUserName());
+        variables.put("tweetContent", tweetProposal.getTweetContent());
+        processEngine.getRuntimeService().startProcessInstanceByKey("Process_TwitterReview", variables);
+        
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/cancel-twitter-review")
+    public ResponseEntity<String> cancelTwitterReview(String tweetToCancel) {
+        processEngine.getRuntimeService()
+            .createMessageCorrelation("MessageCancelTwitterReview")
+            .processInstanceVariableEquals("tweetContent", tweetToCancel)
+            .correlate();
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+}
---
+++ processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/TweetProposal.java
@@ -0,0 +1,21 @@
+package ch.zhaw.gpi.twitterreviewprocessapplication;
+
+public class TweetProposal {
+    private String userName;
+    private String tweetContent;
+    
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getTweetContent() {
+        return tweetContent;
+    }
+    public void setTweetContent(String tweetContent) {
+        this.tweetContent = tweetContent;
+    }
+
+    
+}
