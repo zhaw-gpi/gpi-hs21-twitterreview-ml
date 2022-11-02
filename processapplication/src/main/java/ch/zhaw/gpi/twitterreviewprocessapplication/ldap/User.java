ef926e0e8964c8e70a39dde2b26cc387c4b89f83
Christian Weber <weei@zhaw.ch>
2022-11-02 09:01:12 +0100

    Semesterwoche 6 (alt 5) abgeschlossen

---
+++ processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/ldap/User.java
@@ -0,0 +1,51 @@
+package ch.zhaw.gpi.twitterreviewprocessapplication.ldap;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class User {
+    @Id
+    private String userName;
+    private String firstName;
+    private String officialName;
+    private String eMail;
+    @ManyToOne
+    private OrgUnit homeOrganization;
+
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getOfficialName() {
+        return officialName;
+    }
+    public void setOfficialName(String officialName) {
+        this.officialName = officialName;
+    }
+    public String geteMail() {
+        return eMail;
+    }
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public OrgUnit getHomeOrganization() {
+        return homeOrganization;
+    }
+    public void setHomeOrganization(OrgUnit homeOrganization) {
+        this.homeOrganization = homeOrganization;
+    }
+
+    
+}
